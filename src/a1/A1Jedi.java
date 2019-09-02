55f6de02e9fd699ec86fd50b971a2dd3391a6373
vivi <viviw@live.unc.edu>
2019-09-02 05:14:33 -0400

    v5

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,14 +9,14 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		int itemsNum = scan.nextInt();
+		int itemsTotal = scan.nextInt();
 		
-		String[] items = new String[itemsNum];
-		double[] cost = new double[itemsNum];
-		int[] itemNum = new int[itemsNum];
-		int[] customer = new int[itemsNum];
+		String[] items = new String[itemsTotal];
+		double[] cost = new double[itemsTotal];
+		int[] itemNum = new int[itemsTotal];
+		int[] customer = new int[itemsTotal];
 		
-		for(int i = 0; i < itemsNum; i++) {
+		for(int i = 0; i < itemsTotal; i++) {
 			items[i] = scan.next();
 			cost[i] = scan.nextDouble();
 		}
@@ -24,15 +24,16 @@ public class A1Jedi {
 		int cusTotal = scan.nextInt();
 		
 		String[] custName = new String[cusTotal];
+		double[] custCost = new double [cusTotal];
 		
 		for (int j = 0; j < cusTotal; j++) {
 			custName[j] = scan.next() + " " +scan.next();
 			int iteamEach = scan.nextInt();
-			int[] index = new int[itemsNum];
+			int[] index = new int[itemsTotal];
 			for (int k =0; k<iteamEach; k++) {
 				int quantity = scan.nextInt();
 				String name = scan.next();
-				for (int h=0; h<itemsNum; h++) {
+				for (int h=0; h<itemsTotal; h++) {
 					if (name.equals(items[h])) {
 						itemNum[h] +=  quantity;
 						index[h]++;
@@ -42,7 +43,7 @@ public class A1Jedi {
 		
 			
 			}
-			for (int l=itemsNum; l > 0 ; l--) {
+			for (int l=0; l < itemsTotal ; l++) {
 				if (index[l] > 0) {
 					customer[l]++;
 				}
@@ -51,7 +52,7 @@ public class A1Jedi {
 		}
 		scan.close();
 		
-		for (int a = 0; a < itemsNum; a++) {
+		for (int a = 0; a < itemsTotal; a++) {
 			if (customer[a] != 0) { 
 				System.out.println(customer[a] + " customers bought " + itemNum[a] +" " + items[a]);
 
